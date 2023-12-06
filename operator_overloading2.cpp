e271c1517dd73cad857a52486aca19c93cce1d1b
KrupaThomas <krupathomas182@gmail.com>
2023-12-06 15:13:40 +0530

    Post increment operator overloading added

---
+++ operator_overloading2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+private:
+    int weight;
+
+public:
+    
+    A(int x = 0)
+    {
+        weight = x;
+    }
+    
+    int operator ++()
+    {
+       return ++weight;
+    
+    }
+    int operator ++ (int)
+    {
+        return weight++;
+    }
+
+    void print()
+    {
+        cout<<"The weight is "<<weight<<endl;
+    }
+    
+};
+
+int main()
+{
+    A obj(24);
+    ++obj;
+    obj.print();
+    obj++;
+    obj.print();
+}
