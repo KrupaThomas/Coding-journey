982b182c20eba2f7b54d14925f1c12aaa0d7e592
KrupaThomas <krupathomas182@gmail.com>
2023-12-06 17:23:17 +0530

    parameterised base class inclusion

--- const_inh.cpp
+++ const_inh.cpp
@@ -7,23 +7,44 @@ class parent
     public :
         parent()
         {
-            cout<<"Default base class";
+            cout<<"Default base class"<<endl;
         }
 
         parent(int x)
         {
-            cout<<"parameterised base class";
+            cout<<"parameterised base class"<<endl;
         }
 };
 
 class child: public parent
 {
 
+    public:
+        /*child()
+        {
+            cout<<"Default child class"<<endl;
+        }
+
+        child(int c)
+        {
+            cout<<"parameterised child class"<<endl;
+        }*/
+
+        child():parent()
+        {
+            cout<<"Default child class"<<endl;
+        }
+
+        child(int c):parent(c)
+        {
+            cout<<"parameterised child class"<<endl;
+        }
 };
 
 int main()
 {
     child mon;
+    child mons(9);
     //child mon(9);
     
     return 0;
